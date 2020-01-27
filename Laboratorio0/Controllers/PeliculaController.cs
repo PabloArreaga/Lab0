6787e03d6d3b7592ffedb6b4c5304168ad9e66a5
PabloArreaga <paal241199@gmail.com>
2020-01-26 20:28:08 -0600

    Se creo el controlador de las peliculas, se creo el Post que agrega un elemento a la pila

---
+++ Laboratorio0/Controllers/PeliculaController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Laboratorio0.Helpers;
+
+namespace Laboratorio0.Controllers
+{
+	[ApiController]
+	[Route("[api/controller]")]
+    public class PeliculaController : ControllerBase
+    {
+       [HttpGet]
+	   [Route("{id?}")]
+	   public IEnumerable<Pelicula> Obtener()
+		{
+			return null;
+		}
+
+		[HttpPost]
+		public Pelicula Agregar ([FromBody]Pelicula pelicula)
+		{
+			Data.Instance.PilaPelicula.Push(pelicula);
+			return pelicula;
+		}
+
+    }
+}
