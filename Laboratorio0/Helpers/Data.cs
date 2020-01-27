3eea0bd655c3357b51bfcd800ffd7891c2b48b04
PabloArreaga <paal241199@gmail.com>
2020-01-26 19:52:36 -0600

    Se creo carpeta helpers y se creo el singleton

---
+++ Laboratorio0/Helpers/Data.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio0.Helpers
+{
+	public class Data
+	{
+		public static Data _instance = null;
+		public static Data Instance
+		{
+			get
+			{
+				if (_instance == null) _instance = new Data();
+				return _instance;
+			}
+		}
+		public Stack<string> PilaPelicula = new Stack<string>();
+	}
+}
